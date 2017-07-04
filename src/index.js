d3d1ede7f947c78d8829e0919ba1e2e186ecebf5
cataway <cataway11@163.com>
2017-07-04 15:01:59 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import Bg from '../assets/images/bg.jpg';
+
+function component() {
+	var element = document.createElement('div');
+
+	element.innerHTML = _.join(['What are you', 'talking about'], ' ');
+	element.classList.add('hello');
+
+	// 添加图片
+	var bgImg = new Image();
+	bgImg.src = Bg;
+
+	element.appendChild(bgImg);
+
+	return element;
+}
+
+document.body.appendChild(component());
