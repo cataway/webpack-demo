9c773fa9ee88304de045a727332adfd3ef91749d
cataway <cataway11@163.com>
2017-07-04 18:00:03 +0800

    update

---
+++ server.js
@@ -0,0 +1,26 @@
+var webpack = require("webpack");
+var webpackDevServer = require("webpack-dev-server");
+var webpackConfig = require("./webpack.config");
+
+// 启动服务
+var compiler = webpack(webpackConfig);
+var server = new webpackDevServer(compiler, {
+	publicPath: webpackConfig.output.publicPath,
+	// proxy: {
+	// 	"/api/*": {
+	// 		target: "https://cnodejs.org",
+	// 		secure: false
+	// 	}
+	// },
+	stats: {
+		colors: true
+	}
+});
+
+server.app.get("*", function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+});
+
+server.listen(8080, function() {
+	console.log("Start success");
+});
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Bg from '../assets/images/bg.jpg';
 function component() {
 	var element = document.createElement('div');
 
-	element.innerHTML = _.join(['What are you', 'talking about'], ' ');
+	element.innerHTML = _.join(['What are you', '弄啥咧？'], ' ');
 	element.classList.add('hello');
 
 	// 添加图片
